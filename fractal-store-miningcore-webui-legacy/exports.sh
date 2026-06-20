0718b18894093a3baa01722b975ddaf33489d481
wvk2017 <58895020+wvk2017@users.noreply.github.com>
2026-06-20 17:08:51 +0200

    Add files via upload

---
+++ fractal-store-miningcore-webui-legacy/exports.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/umbrel/.miningcore_legacy
+mkdir -p /home/umbrel/umbrel/app-data/retro-mike-vtc-node/.vertcoin
+chown -R 1000:1000 /home/umbrel/.miningcore_legacy
+chown -R 1000:1000 /home/umbrel/umbrel/app-data/retro-mike-vtc-node/.vertcoin
